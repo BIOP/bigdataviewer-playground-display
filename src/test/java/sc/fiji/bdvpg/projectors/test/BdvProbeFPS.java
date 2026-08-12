17ffe5648d46346dac89994a35781ec75f8c5162
Nicolas Chiaruttini <nicolas.chiaruttini@unige.ch>
2026-08-12 09:34:31 +0200

    Adapts to new pom scijava

--- src/test/java/sc/fiji/bdvpg/projectors/test/BdvProbeFPS.java
+++ src/test/java/sc/fiji/bdvpg/projectors/test/BdvProbeFPS.java
@@ -64,8 +64,7 @@ public class BdvProbeFPS {
 		}
 		final MyTarget target = new MyTarget();
 		final MultiResolutionRenderer renderer = new MultiResolutionRenderer(target,
-			() -> {}, new double[] { 1 }, 0, 1, null, false, viewer.getOptionValues()
-				.getAccumulateProjectorFactory(), new CacheControl.Dummy());
+			() -> {}, new double[] { 1 }, 0, 1, null, false, new CacheControl.Dummy());
 		// progressWriter.setProgress( 0 );
 		for (int timepoint = 0; timepoint <= nFrames; ++timepoint) {
 			// renderState.setCurrentTimepoint( timepoint );
